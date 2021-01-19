035077f79c36e9347df69c54560ee1abb514434f
EKaxada <enocksdha@gmail.com>
2021-01-19 12:53:10 +0300

    removed frontend

--- client/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
-
-app.use(express.static(__dirname))
-app.listen(3000, ()=>console.log('App running on port 3000'))
