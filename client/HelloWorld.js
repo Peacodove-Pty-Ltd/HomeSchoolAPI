073039d1ee7a4349f64c509444d8e04af59a226c
EKaxada <enocksdha@gmail.com>
2021-01-19 11:34:35 +0300

    connected backend to frontend

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from "axios"
 
 const HelloWorld = () => {
+  
+  axios.get('http://localhost:8000/api')
+  .then(res=>{console.log(res.data); console.log(res.status)});
+
   return (
     <div>
       <h1>hello world</h1>
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
+app.use('/api', (req, res) => {
+  res.status(200).json({api: 'version 1'})
+})
 
 // Database Connection URL
 const db = "mongodb+srv://EKaxada:1234567890@cluster0.3bhqr.mongodb.net/EKaxada?retryWrites=true&w=majority";
@@ -13,7 +18,7 @@ const db = "mongodb+srv://EKaxada:1234567890@cluster0.3bhqr.mongodb.net/EKaxada?
 
 mongoose.connect(db).then(()=>console.log("db connected ..")).catch(err=>console.log(err))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, function onStart(err) {
   if (err) {
     console.log(err);
