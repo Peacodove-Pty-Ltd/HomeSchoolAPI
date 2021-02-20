286dd29e743e5ce787811bdeab295a62fb5eb10c
jnsereko <admin@jnsereko.com>
2021-02-20 14:02:55 +0300

    modified database to match that in design database

--- src/routes/ClassRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let ClassRoutes = require("../models/ClassModel");
-
-
-
-module.exports = router;
--- src/routes/HomeworkResultsRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-let HomeworkResults = require("../models/HomeworkResultsModel ");
-
-
-
-module.exports = router;
