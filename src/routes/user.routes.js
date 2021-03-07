f6030d8342e099cf94099e6bad365645bb9393f9
Enoch Kasaadha <enocksdha@gmail.com>
2021-03-07 12:26:39 +0300

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,67 +2,19 @@ const express = require('express');
 const router = express.Router();
 let User = require("../models/user.model");
 
-/**
-  * getById
-  *   @description CRUD ACTION get
-  *   @param ObjectId id Id resource
-  *
-  */
-router.route("/:id").get((req, res) => {
-    const id = req.params.id;
-    User.findOne({ _id : id })
-        .then(user => res.json(user))
-        .catch(err => res.status(400).json("Error getting element with id: "+id+" " + err));
-});
 
-/**
-  * delete
-  *   @description CRUD ACTION delete
-  *   @param ObjectId id Id
-  *
-  */
-router.route("/delete/:id").get((req, res) => {
-    const id = req.params.id;
-    User.findByIdAndRemove(id)
-        .then(() => res.json("element with id: "+req.body.id+ " was deleted"))
-        .catch(err => res.status(400).json("Error deleting element with id: "+req.body.id+ " "+ err));
-});
-
-/**
-  * create
-  *   @description CRUD ACTION create
-  *
-  */
-router.route('/add').post((req, res) => {
-    const item = req.body;
-    const newUser = new User({item});
-    newUser.save()
-        .then(()=>res.json("you were added to the waiting list"))
-        .catch(err => res.status(400).json("Error adding element to the waiting list: " + err))
-});
-
-/**
-  * list
-  *   @description CRUD ACTION list
-  *
-  */
-router.route("").get((req, res) => {
+router.route("/").get((req, res) => {
     User.find()
         .then(users => res.json(users))
-        .catch(err => res.status(400).json("Error getting elements: " + err));
+        .catch(err => res.status(400).json("Error: " + err));
 });
 
-/**
-  * update
-  *   @description CRUD ACTION update
-  *   @param ObjectId id Id
-  *
-  */
- router.route("/:id").get((req, res) => {
-     const item = req.body;
-    User.findOneAndUpdate({ _id: item._id }, item, {'new': true})
-        .then(user => res.json(user))
-        .catch(err => res.status(400).json("Error getting element: " + err));
+router.route('/add').post((req, res) => {
+    const email = req.body.email;
+    const newUser = new User({email});
+    newUser.save()
+        .then(()=>res.json("you were added to the waiting list"))
+        .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
