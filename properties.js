e2e4d48837dd521ed0f3f66a9e38a29bf32613eb
EKaxada <enocksdha@gmail.com>
2021-03-07 11:26:34 +0300

    deleted redundant files

--- properties.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	Peacodove_db_dbUrl: (process.env.DB_HOST || 'localhost') + ':27017/Peacodove_db',
-	port: process.env.NODE_PORT || 3000,
-    tokenSecret: 'Insert Your Secret Token',
-    api: process.env.NODE_API != null ? process.env.NODE_API : '/api'
-}
