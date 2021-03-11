c43250373b2dde1d767a0b13928c9a8d8250d2d5
EKaxada <enocksdha@gmail.com>
2021-03-11 21:15:41 +0300

    connected routes to server

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,18 @@
-const express = require('express');
+const express = require( 'express' );
+
 const router = express.Router();
 
-//import user controllers
-import {signupUser, loginUser} from "../controllers/user.controller"
+// import user controllers
+const userRoutes =require( "../controllers/user.controller" ); 
 
 // @route POST api/users/signup
 // @desc signup new user
 // @access PUBLIC
-router.post("/signup", signupUser )
+router.post( "/signup", userRoutes.signupUser );
 
 // @route POST api/users/login
 // @desc login new user
 // @access PUBLIC
-router.post("/login", loginUser )
+router.post( "/login", userRoutes.loginUser );
 
 module.exports = router;
